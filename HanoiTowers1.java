8da69c47ff3a89ea9dc764e3e637d4d2cf5dcc64
maciek-so <54718619+maciek-so@users.noreply.github.com>
2019-09-06 18:44:45 +0200

    Hanoi game

---
+++ HanoiTowers1.java
@@ -0,0 +1,162 @@
+import java.util.Scanner;
+
+class Wieze {
+int tab[][];
+int kije;
+int krazki;
+int pwolne[];
+	
+	Wieze() {
+		kije=3;
+		krazki=4;
+ 		tab = new int[kije][krazki];
+		pwolne= new int[kije];
+	}
+	
+	void printCurrent() {
+		System.out.println("");
+		for(int i=krazki; i>0; i--) {
+			for(int j=0; j<kije; j++) {
+				if(tab[j][i-1]==0) 
+				System.out.print("  |");
+				else
+				System.out.print("  "+tab[j][i-1]);
+			}
+			System.out.println("");
+		}
+
+			System.out.println("-----------");
+			System.out.println("  1  2  3  ");
+			System.out.println("##"+pwolne[0]+"##"+pwolne[1]+"##"+pwolne[2]+"##");
+	}
+	
+	String przeloz(int z, int na) {
+		System.out.println("Przenosisz krazek z palika "+z+" na palik "+na+"...");
+		if(pwolne[z-1]<=0) {
+			System.out.println("Nie mozna zdjac krazka z pustego palika!");
+			return "Zablokowano błąd użytkownika!";
+		}
+
+		if (pwolne[na-1]>0) {
+			if (tab[z-1][pwolne[z-1]-1]>tab[na-1][pwolne[na-1]-1]) {
+				System.out.println("Nie mozna ukladac wiekszego krazka na mniejszym");
+				return "Zablokowano błąd użytkownika!";
+			}
+		}
+
+		if(pwolne[na-1]>=krazki) {
+			System.out.println("Palik przepelniony!");
+			return "Zablokowano błąd użytkownika!";
+		}
+		tab[na-1][pwolne[na-1]]=tab[z-1][pwolne[z-1]-1];
+		tab[z-1][pwolne[z-1]-1]=0;
+		pwolne[z-1]--;
+		pwolne[na-1]++;
+		if(pwolne[1]==4 || pwolne[1]==4) {
+			System.out.println("##################");
+			System.out.println("#                #");
+			System.out.println("#   YOU WIN :)   #");
+			System.out.println("#                #");
+			System.out.println("##################");
+			return "Wygrana!!!";
+		}
+		this.printCurrent();
+		System.out.println("Po przelozeniu plansza wyglada jak powyzej.\n");
+		return "OK!";
+	} 
+}
+
+
+// główny program
+class HanoiTowers1 {
+  public static void main(String args[]) {
+	
+	int n=3;
+	String st;
+	char ch;
+    Scanner in = new Scanner(System.in);
+
+	Wieze w = new Wieze();
+	w.tab[0][0]=4;
+	w.tab[0][1]=3;
+	w.tab[0][2]=2;
+	w.tab[0][3]=1;
+	w.pwolne[0]=4;
+
+
+	
+	System.out.println("Aby rozpoczac nacisnij 's'.");
+	st = in.nextLine();
+	ch = st.charAt(0);
+	if(ch == 's' || ch == 'S') {
+
+		//poprawnie uruchomiona gra:
+		int z = 0;
+		int na = 0;
+		boolean inAccepted = false;
+		System.out.println("Wstep... Instrukcja");
+		System.out.println("Aby opuscic gre w dowolnym momencie wprowadz 'q'.");
+		w.printCurrent();
+		while(ch!='q') {		
+		while(inAccepted == false) {
+			System.out.println("Z ktorego palika chcesz przelozyc krazek?");
+			st = in.nextLine();
+			ch = st.charAt(0);
+			if(ch=='q') return;
+			switch(ch) {
+			case '1':
+				inAccepted = true;
+				break;
+			case '2':
+				inAccepted = true;
+				break;
+			case '3':
+				inAccepted = true;
+				break;
+			default:
+				System.out.println("Ogarnij sie i wpisz poprawny numer palika.");
+				break;
+		}
+		}
+		inAccepted = false;
+		switch(ch) {
+			case '1':
+				z=1;
+				break;
+			case '2':
+				z=2;
+				break;
+			case '3':
+				z=3;
+				break;
+			default:
+				System.out.println("No wez sie!");
+				break;
+		}
+		System.out.println("Na ktory palik chcesz przelozyc krazek?");
+		st = in.nextLine();
+		ch = st.charAt(0);
+		if(ch=='q') return;
+		switch(ch) {
+			case '1':
+				na=1;
+				break;
+			case '2':
+				na=2;
+				break;
+			case '3':
+				na=3;
+				break;
+			default:
+				System.out.println("No wez sie!");
+				break;
+		}
+		w.przeloz(z,na);
+	}
+	}
+	else {
+		System.out.println("Nie wyglupiaj sie, '"+ch+"' to nie 's'");
+		System.out.println("Game over");
+	}
+  }
+}
